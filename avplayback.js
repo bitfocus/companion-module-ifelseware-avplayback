00b14afe3c9d7b88588652f1c64e635daa89ace7
Jeffrey Davidsz <jeffrey.davidsz@vicreo.eu>
2021-08-02 08:46:13 +0200

    Chore: prettier

--- avplayback.js
+++ avplayback.js
@@ -1,64 +1,64 @@
-var instance_skel = require('../../instance_skel');
-var udp           = require('../../udp');
-var debug;
-var log;
+var instance_skel = require('../../instance_skel')
+var udp = require('../../udp')
+var debug
+var log
 
 function instance(system, id, config) {
-	var self = this;
+	var self = this
 
 	// super-constructor
-	instance_skel.apply(this, arguments);
+	instance_skel.apply(this, arguments)
 
-	self.actions(); // export actions
+	self.actions() // export actions
 
-	return self;
+	return self
 }
 
-instance.prototype.init = function() {
-	var self = this;
+instance.prototype.init = function () {
+	var self = this
 
-	debug = self.debug;
-	log = self.log;
+	debug = self.debug
+	log = self.log
 
-	self.status(self.STATUS_UNKNOWN);
+	self.status(self.STATUS_UNKNOWN)
 
 	if (self.config.host !== undefined) {
-		self.udp = new udp(self.config.host, self.config.port);
+		self.udp = new udp(self.config.host, self.config.port)
 
 		self.udp.on('status_change', function (status, message) {
-			self.status(status, message);
-		});
+			self.status(status, message)
+		})
 	}
-};
+}
 
-instance.prototype.updateConfig = function(config) {
-	var self = this;
-	self.config = config;
+instance.prototype.updateConfig = function (config) {
+	var self = this
+	self.config = config
 
 	if (self.udp !== undefined) {
-		self.udp.destroy();
-		delete self.udp;
+		self.udp.destroy()
+		delete self.udp
 	}
 
 	if (self.config.host !== undefined) {
-		self.udp = new udp(self.config.host, self.config.port);
+		self.udp = new udp(self.config.host, self.config.port)
 
 		self.udp.on('status_change', function (status, message) {
-			self.status(status, message);
-		});
+			self.status(status, message)
+		})
 	}
-};
+}
 
 // Return config fields for web config
 instance.prototype.config_fields = function () {
-	var self = this;
+	var self = this
 	return [
 		{
 			type: 'textinput',
 			id: 'host',
 			label: 'Target IP',
 			width: 6,
-			regex: self.REGEX_IP
+			regex: self.REGEX_IP,
 		},
 		{
 			type: 'textinput',
@@ -66,33 +66,34 @@ instance.prototype.config_fields = function () {
 			label: 'Target Port',
 			width: 6,
 			default: 7000,
-			regex: self.REGEX_PORT
+			regex: self.REGEX_PORT,
 		},
 		{
 			type: 'text',
 			id: 'info',
 			label: 'Information',
 			width: 12,
-			value: 'AV-Playback is cost effective Windows based media play-out solution that offers a full list of robust features any professional playback operator will find indispensable.</br></br><a href="https://www.ifelseware.com" target="_new">Learn more about AV-Playback and our other pro audio visual solutions.</a>'
-		}
+			value:
+				'AV-Playback is cost effective Windows based media play-out solution that offers a full list of robust features any professional playback operator will find indispensable.</br></br><a href="https://www.ifelseware.com" target="_new">Learn more about AV-Playback and our other pro audio visual solutions.</a>',
+		},
 	]
-};
+}
 
 // When module gets deleted
-instance.prototype.destroy = function() {
-	var self = this;
+instance.prototype.destroy = function () {
+	var self = this
 
 	if (self.udp !== undefined) {
-		self.udp.destroy();
+		self.udp.destroy()
 	}
-	debug("destroy", self.id);
-};
+	debug('destroy', self.id)
+}
 
-instance.prototype.actions = function(system) {
-	var self = this;
+instance.prototype.actions = function (system) {
+	var self = this
 
 	self.system.emit('instance_actions', self.id, {
-		'load': {
+		load: {
 			label: 'Load Clip (ID)',
 			options: [
 				{
@@ -100,12 +101,12 @@ instance.prototype.actions = function(system) {
 					label: 'Clip ID',
 					id: 'clip',
 					default: 1,
-					regex: self.REGEX_NUMBER
-				}
-			]
+					regex: self.REGEX_NUMBER,
+				},
+			],
 		},
 
-		'playClip': {
+		playClip: {
 			label: 'Load & Play Clip (ID)',
 			options: [
 				{
@@ -113,29 +114,29 @@ instance.prototype.actions = function(system) {
 					label: 'Clip ID',
 					id: 'clip',
 					default: 1,
-					regex: self.REGEX_NUMBER
-				}
-			]
+					regex: self.REGEX_NUMBER,
+				},
+			],
 		},
 
-		'play':     		{ label: 'Program Play' },
-		'pause':    		{ label: 'Program Pause' },
-		'stop':     		{ label: 'Program Stop' },
-		'previewStart':     { label: 'Preview Play' },
-		'previewPause':    	{ label: 'Preview Pause' },
-		'previewStop':     	{ label: 'Preview Stop' },
-		'nextClip':			{ label: 'Next Clip'},
-		'prevClip':			{ label: 'Previous Clip'},
-		'freeze':   		{ label: 'Freeze temp' },
-		'loop':     		{ label: 'Loop temp' },
-		'nextTag':   		{ label: 'Next Tag' },
-		'prevTag':     		{ label: 'Previous Tag' },
-		'10':       		{ label: 'Goto 10' },
-		'20':       		{ label: 'Goto 20' },
-		'30':       		{ label: 'Goto 30' },
-		'60':				{ label: 'Goto 60' },
+		play: { label: 'Program Play' },
+		pause: { label: 'Program Pause' },
+		stop: { label: 'Program Stop' },
+		previewStart: { label: 'Preview Play' },
+		previewPause: { label: 'Preview Pause' },
+		previewStop: { label: 'Preview Stop' },
+		nextClip: { label: 'Next Clip' },
+		prevClip: { label: 'Previous Clip' },
+		freeze: { label: 'Freeze temp' },
+		loop: { label: 'Loop temp' },
+		nextTag: { label: 'Next Tag' },
+		prevTag: { label: 'Previous Tag' },
+		10: { label: 'Goto 10' },
+		20: { label: 'Goto 20' },
+		30: { label: 'Goto 30' },
+		60: { label: 'Goto 60' },
 
-		'gotoTc': {
+		gotoTc: {
 			label: 'Goto (TimeCode)',
 			options: [
 				{
@@ -143,114 +144,109 @@ instance.prototype.actions = function(system) {
 					label: 'hh:mm:ss:ff',
 					id: 'tc',
 					default: '00:00:00:00',
-					regex: self.REGEX_TIMECODE
-				}
-			]
+					regex: self.REGEX_TIMECODE,
+				},
+			],
 		},
-	});
-};
+	})
+}
 
-
-instance.prototype.action = function(action) {
-	var self = this;
-	var id = action.action;
-	var cmd;
-	var opt = action.options;
+instance.prototype.action = function (action) {
+	var self = this
+	var id = action.action
+	var cmd
+	var opt = action.options
 
 	// avplayback default port 7000
 	switch (action.action) {
-
 		case 'load':
-			cmd = 'AVP|1|LoadClip,' + ('0' + opt.clip).substr(-2);
-			break;
+			cmd = 'AVP|1|LoadClip,' + ('0' + opt.clip).substr(-2)
+			break
 
 		case 'playClip':
-			cmd = 'AVP|1|PgmStart,' + ('0' + opt.clip).substr(-2);
-			break;
+			cmd = 'AVP|1|PgmStart,' + ('0' + opt.clip).substr(-2)
+			break
 
 		case 'play':
-			cmd = 'AVP|1|PgmStart,-1';
-			break;
+			cmd = 'AVP|1|PgmStart,-1'
+			break
 
 		case 'pause':
-			cmd = 'AVP|1|PgmPause';
-			break;
+			cmd = 'AVP|1|PgmPause'
+			break
 
 		case 'nextClip':
-			cmd = 'AVP|1|NextClip';
-			break;
+			cmd = 'AVP|1|NextClip'
+			break
 
 		case 'stop':
-			cmd = 'AVP|1|PgmStop';
-			break;
+			cmd = 'AVP|1|PgmStop'
+			break
 
 		case 'previewStart':
-			cmd = 'AVP|1|PrvStart';
-			break;
+			cmd = 'AVP|1|PrvStart'
+			break
 
 		case 'previewPause':
-			cmd = 'AVP|1|PrvPause';
-			break;
+			cmd = 'AVP|1|PrvPause'
+			break
 
 		case 'previewStop':
-			cmd = 'AVP|1|PrvStop';
-			break;
+			cmd = 'AVP|1|PrvStop'
+			break
 
 		case 'nextClip':
-			cmd = 'AVP|1|NextClip';
-			break;
+			cmd = 'AVP|1|NextClip'
+			break
 
 		case 'prevClip':
-			cmd = 'AVP|1|PrevClip';
-			break;
+			cmd = 'AVP|1|PrevClip'
+			break
 
 		case 'freeze':
-			cmd = 'AVP|1|TmpHold';
-			break;
+			cmd = 'AVP|1|TmpHold'
+			break
 
 		case 'loop':
-			cmd = 'AVP|1|TmpLoop';
-			break;
+			cmd = 'AVP|1|TmpLoop'
+			break
 
 		case 'nextTag':
-			cmd = 'AVP|1|NextTag';
-			break;
+			cmd = 'AVP|1|NextTag'
+			break
 
 		case 'prevTag':
-			cmd = 'AVP|1|PrevTag';
-			break;
+			cmd = 'AVP|1|PrevTag'
+			break
 
 		case 'gotoTc':
-			cmd = 'AVP|1|SetPosition,'+ opt.tc;
-			break;
+			cmd = 'AVP|1|SetPosition,' + opt.tc
+			break
 
 		case '10':
-			cmd = 'AVP|1|GotoTimeOut,4';
-			break;
+			cmd = 'AVP|1|GotoTimeOut,4'
+			break
 
 		case '20':
-			cmd = 'AVP|1|GotoTimeOut,3';
-			break;
+			cmd = 'AVP|1|GotoTimeOut,3'
+			break
 
 		case '30':
-			cmd = 'AVP|1|GotoTimeOut,2';
-			break;
+			cmd = 'AVP|1|GotoTimeOut,2'
+			break
 
 		case '60':
-			cmd = 'AVP|1|GotoTimeOut,1';
-			break;
+			cmd = 'AVP|1|GotoTimeOut,1'
+			break
 	}
 
 	if (cmd !== undefined) {
-
 		if (self.udp !== undefined) {
-
-			debug('sending ',cmd,"to",self.udp.host);
-			self.udp.send(cmd);
+			debug('sending ', cmd, 'to', self.udp.host)
+			self.udp.send(cmd)
 		}
 	}
-};
+}
 
-
-instance_skel.extendedBy(instance);
-exports = module.exports = instance;
+instance_skel.extendedBy(instance)
+exports = module.exports = instance
